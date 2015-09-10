c04222778ee475f217c939f9eade7bdd3484b0b5
AniahLiepard <aniah.garcia@techhigh.us>
2015-09-10 11:49:06 -0400

    Conditional Operators Project
    https://www.thenewboston.com/videos.php?cat=31&amp;video=17985

---
+++ ProjectSeventeen.java
@@ -0,0 +1,7 @@
+public class ProjectSeventeen {
+	public static void main(String args[]){
+		int line = 4;
+		
+		System.out.println(line > 1 ? "Congratulations...... you made a rectangle." : "Are we making circles now?");
+	}
+}
